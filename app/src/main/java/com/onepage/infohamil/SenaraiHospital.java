86d06650fd7636bdd63b27244eb05b99f250c9c7
SeladangHijau <nadzmiidzham@gmail.com>
2017-04-28 08:36:21 +0600

    #1 - added new graphic for minggu hamil - added 3 new main menu

--- app/src/main/java/com/onepage/infohamil/SenaraiHospital.java
+++ app/src/main/java/com/onepage/infohamil/SenaraiHospital.java
@@ -1,11 +1,11 @@
 package com.onepage.infohamil;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,28 +14,30 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.onepage.infohamil.adapter.ListViewMenuAdapter;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class SenaraiHospital extends AppCompatActivity implements AdapterView.OnItemClickListener {
     DrawerLayout dlMain;
-    ListView lvSettingMenu, lvMainMenu;
+    ListView lvSettingMenu, lvSenaraiNegeri;
     TextView tvActTitle; // actionbar title
 
-    private String[] settingMenu, mainMenu;
+    private String[] settingMenu, senaraiNegeri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_senarai_hospital);
 
         // call setup methods
         setup();
+        setupActionBar();
         setupViews();
         setupListener();
     }
 
     // setup methods
     private void setup() {
-        mainMenu = getResources().getStringArray(R.array.menu_utama);
+        senaraiNegeri = getResources().getStringArray(R.array.menu_senarai_negeri);
         settingMenu = getResources().getStringArray(R.array.menu_setting);
     }
 
@@ -59,41 +61,44 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         tvActTitle = (TextView) findViewById(R.id.tvActTitle);
 
         ibShare.setVisibility(View.INVISIBLE);
-        tvActTitle.setText(R.string.app_name);
+        tvActTitle.setText(R.string.tajuk_act_info_minggu_hamil);
 
         ibMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (dlMain.isDrawerVisible(GravityCompat.START))
-                    dlMain.closeDrawer(GravityCompat.START);
-                else
-                    dlMain.openDrawer(GravityCompat.START);
+                if(dlMain != null)
+                    if (dlMain.isDrawerVisible(GravityCompat.START))
+                        dlMain.closeDrawer(GravityCompat.START);
+                    else
+                        dlMain.openDrawer(GravityCompat.START);
             }
         });
     }
 
     private void setupViews() {
-        setupActionBar();
+        ListViewMenuAdapter menuAdapter;
+
+        menuAdapter = new ListViewMenuAdapter(this, senaraiNegeri);
 
         dlMain = (DrawerLayout) findViewById(R.id.dlMain);
-        lvMainMenu = (ListView) findViewById(R.id.lvMainMenu);
         lvSettingMenu = (ListView) findViewById(R.id.lvSettingMenu);
+        lvSenaraiNegeri = (ListView) findViewById(R.id.lvSenaraiNegeri);
 
-        lvMainMenu.setAdapter(new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, mainMenu));
+        lvSenaraiNegeri.setAdapter(menuAdapter);
         lvSettingMenu.setAdapter(new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, settingMenu));
     }
 
     private void setupListener() {
-        lvMainMenu.setOnItemClickListener(this);
+        lvSenaraiNegeri.setOnItemClickListener(this);
         lvSettingMenu.setOnItemClickListener(this);
     }
 
-    // Listeners
+    // listener
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         switch(parent.getId()) {
             case R.id.lvSettingMenu:
-                switch (position) {
+                switch(position) {
                     case 0:
                         finish();
                         startActivity(new Intent(this, MainActivity.class));
@@ -103,28 +108,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     case 2:
                         break;
                 }
-                dlMain.closeDrawers();
                 break;
-            case R.id.lvMainMenu:
-                switch (position) {
-                    case 0:
-                        startActivity(new Intent(this, TandaHamil.class));
-                        break;
-                    case 1:
-                        break;
-                    case 2:
-                        startActivity(new Intent(this, MingguHamil.class));
-                        break;
-                    case 3:
-                        startActivity(new Intent(this, TipsPenjagaanIbu.class));
-                        break;
-                    case 4:
-                        startActivity(new Intent(this, TipsPenjagaanBayi.class));
-                        break;
-                    case 5:
-                        startActivity(new Intent(this, SenaraiHospital.class));
-                        break;
-                }
+            case R.id.lvSenaraiNegeri:
+                // TODO: 4/15/2017 - implement link
                 break;
         }
     }
